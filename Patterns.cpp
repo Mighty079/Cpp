828cc34d64bd7315ab7448ebb763e9c74c0cb721
Krishnaditya <krishnaditya004@gmail.com>
2025-11-02 11:24:51 +0530

    Add files via upload

--- Patterns.cpp
+++ Patterns.cpp
@@ -39,8 +39,37 @@ int pattern4(){
 cout<<endl;
     }
 }
+int pattern5(){
+    int i,j,n=4;
+    for(i=0;i<n;i++){
+        for(j=0;j<i;j++){
+            cout<<" ";
+        }
+            for(j=0;j<n-i;j++){
+                cout<<i+1;
+    }    
+cout<<endl;
+    
+}
+}
+int pattern6(){
+    int i,j,n=4;
+    for(i=0;i<n;i++){
+        for(j=0;j<n-i;j++){
+            cout<<" ";
+        }
+            for(j=0;j<2*i+1;j++){
+                cout<<"*";
+            }
+          cout<<endl;       
+
+        } 
+}
+
+
+
 int main(){
     
-    pattern4();
+    pattern6();
   
 }
